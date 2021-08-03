2f7e5b01b9a007433155f1fa43a3588246b8c953
Nguyễn Hiếu <vanhieu13546@gmail.com>
2021-08-03 16:42:54 +0700

    Init

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  express = require("express"),
+  cors = require("cors"),
+  app = express();
+
+// --------------------------------------------------------------------
+// APP CONFIG
+// --------------------------------------------------------------------
+app
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }));
+
+// --------------------------------------------------------------------
+// MONGODB/MONGOOSE
+// --------------------------------------------------------------------
+const MONGODB_URI = "mongodb://localhost:27017/avatar-db";
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to URL " + MONGODB_URI))
+  .catch((err) => console.log("Error on db connection: " + err.message));
+
+// --------------------------------------------------------------------
+// ROUTES
+// --------------------------------------------------------------------
+const AvatarRouter = require("./routes/avatar");
+
+app.use("/", AvatarRouter);
+
+// --------------------------------------------------------------------
+// SERVER LISTENER
+// --------------------------------------------------------------------
+
+app.listen(3000, () => console.log("Server listening on port 3000!"));
